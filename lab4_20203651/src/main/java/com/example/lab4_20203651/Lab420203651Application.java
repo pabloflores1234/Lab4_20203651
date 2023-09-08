5a25a4892d1a2cad5234d259b724785aa2047b93
Pablo <a20203651@pucp.edu.pe>
2023-09-07 20:46:07 -0500

    creando archivo

---
+++ lab4_20203651/src/main/java/com/example/lab4_20203651/Lab420203651Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20203651;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420203651Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420203651Application.class, args);
+	}
+
+}
---
+++ lab4_20203651/src/test/java/com/example/lab4_20203651/Lab420203651ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20203651;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420203651ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
